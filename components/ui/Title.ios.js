ca877c45bc3ed708959c5c704b9596ee76c69113
Oscar Martinez <oscar1793@hotmail.com>
2023-03-12 02:04:24 -0600

    Styles modified to make a responsive multiplatform user interfaces

---
+++ components/ui/Title.ios.js
@@ -0,0 +1,29 @@
+import { Text, StyleSheet, Platform } from "react-native";
+
+const Title = ({ children }) => {
+  return <Text style={styles.title}>{children}</Text>;
+};
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 24,
+    color: "white",
+    textAlign: "center",
+    // ...Platform.select({
+    //   android: {
+    //     borderWidth: 2,
+    //   },
+    //   ios: {
+    //     borderWidth: 0,
+    //   },
+    // }),
+    //borderWidth: Platform.OS === "android" ? 2 : 0,
+    //borderWidth: Platform.select({ ios: 0, android: 2 }),
+    padding: 12,
+    maxWidth: "80%",
+    width: 300,
+  },
+});
